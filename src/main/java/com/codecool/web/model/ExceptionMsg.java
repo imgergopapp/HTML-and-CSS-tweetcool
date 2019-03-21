d452e88120b456c0f30f8d4e8a9fc4421ba74fd0
Gergő Papp <imgergopapp@gmail.com>
2019-03-21 11:28:08 +0100

    Stores costum Exception message

---
+++ src/main/java/com/codecool/web/model/ExceptionMsg.java
@@ -0,0 +1,16 @@
+package com.codecool.web.model;
+
+public class ExceptionMsg {
+    private static String msg;
+
+    public static String getMsg() {
+        return msg;
+    }
+
+    public static void setMsg(String message) {
+        msg = message;
+    }
+
+
+
+}
