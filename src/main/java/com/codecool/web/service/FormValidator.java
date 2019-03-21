16e12dd99b98f438e07553a89d176d25538b75ae
Gergő Papp <imgergopapp@gmail.com>
2019-03-21 11:31:57 +0100

    Removed comment

--- src/main/java/com/codecool/web/service/FormValidator.java
+++ src/main/java/com/codecool/web/service/FormValidator.java
@@ -2,9 +2,8 @@ package com.codecool.web.service;
 
 public class FormValidator {
 
-    public static void formCheck(String s1, String s2){
-       // return poster.isEmpty() || content.isEmpty();
-        if(s1.isEmpty() || s2.isEmpty()){
+    public static void formCheck(String s1, String s2) {
+        if (s1.isEmpty() || s2.isEmpty()) {
             throw new InvalidFormException("Invalid Form. Fill both name & tweet fields.");
         }
     }
