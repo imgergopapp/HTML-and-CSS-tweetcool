e6134fc91a66f3c96dd0d240258711cc0034ceb2
Gergő Papp <imgergopapp@gmail.com>
2019-03-21 11:42:24 +0100

    Added imports, exception handling

--- src/main/java/com/codecool/web/servlet/WelcomeServlet.java
+++ src/main/java/com/codecool/web/servlet/WelcomeServlet.java
@@ -1,5 +1,9 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.model.ExceptionMsg;
+import com.codecool.web.model.Tweet;
+import com.codecool.web.service.XmlParser;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,24 +11,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet({"","/welcome"})
+@WebServlet({"", "/welcome"})
 public class WelcomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       // req.getRequestDispatcher("welcome.jsp").forward(req, resp);
-        //resp.sendRedirect("welcome.jsp");
-        req.getRequestDispatcher("welcome.jsp").forward(req, resp);
+        String path = req.getServletContext().getRealPath("data.xml");
+        String redirectUrl = "welcome.jsp";
+        try {
+            Tweet.setTweetList(XmlParser.read(path));
+        } catch (IOException ioe) {
+            String msg = "Invalid data.xml! Make sure it's in the correct folder!";
+            ExceptionMsg.setMsg(msg);
 
+            redirectUrl="error";
+        }
+        catch (Exception e){
+            String msg = "XmlParser Exception! ";
+            ExceptionMsg.setMsg(msg);
+            redirectUrl="error";
+        }
+
+        req.getRequestDispatcher(redirectUrl).forward(req, resp);
     }
-
- /*   @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-
-
-        request.getRequestDispatcher("welcome.jsp").forward(request, response);
-    }*/
 }
 
